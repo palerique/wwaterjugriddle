fbd9b71cd804726a8fa30e87456104821b58ecd5
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2024-06-09 18:25:16 -0300

    feat: add kubernetes deployment files and update build script
    The commit introduces Kubernetes deployment files for the 'api' and 'web' applications. The build-and-run-docker-image.sh script has been updated to include the building of Docker images for 'api' and 'web' apps, and deploys them to Kubernetes. Istio installation and port forwarding for observability tools have also been added to the script.

--- build-and-run-docker-image.sh
+++ build-and-run-docker-image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e  # fail on any error
+set -x  # display commands being run
 
 # Check if docker is installed
 if ! command -v docker &> /dev/null
@@ -9,6 +10,60 @@ then
 fi
 
 # Build the Docker image
-docker build -f sheep-monolith/Dockerfile -t sheep-monolith:1.0.0 .
+docker build -f apps/api/Dockerfile -t br.com.palerique/water-riddle-api:latest . & \
+docker build -f apps/web/Dockerfile -t br.com.palerique/water-riddle-web:latest .
 
-#docker run -p 8080:8080 sheep-monolith:1.0.0
+
+# Istio / Kubernetes configuration:
+
+#!/usr/bin/env bash
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo update
+
+# Install Istio
+istioctl install --set profile=demo -y
+
+# Loop until all Istio pods are running or have completed
+while : ; do
+  [[ $(kubectl get pods -n istio-system 2>&1) =~ "No resources found" ]] && sleep 1 && continue
+  readyPods=$(kubectl get pods -n istio-system --no-headers 2>/dev/null | grep -v -E '(Running|Completed)' || true)
+  if [[ -z "$readyPods" ]]; then
+    break
+  fi
+  echo "Waiting for Istio pods to be ready..."
+  sleep 5
+done
+
+kubectl label namespace default istio-injection=enabled
+
+#helm install prometheus bitnami/kube-prometheus --wait
+#helm install grafana bitnami/grafana --wait
+
+#helm install jaegertracing jaegertracing/jaeger -n observability --wait
+#helm install jaegertracing jaegertracing/jaeger --wait
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/kiali.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/jaeger.yaml
+
+# Deploy the application to Kubernetes:
+kubectl apply -f apps/api/k8s/api.yaml
+kubectl apply -f apps/web/k8s/web.yaml
+
+#Forward the ports for the services:
+kubectl port-forward --namespace istio-system svc/prometheus 9090:9090 &
+echo $! > /tmp/prometheus-pid
+
+kubectl port-forward --namespace istio-system svc/grafana 8180:3000 &
+echo $! > /tmp/grafana-pid
+
+kubectl port-forward --namespace istio-system svc/kiali 8380:20001 &
+echo $! > /tmp/kiali-pid
+
+POD_NAME="$(kubectl get pods --namespace istio-system -l "app=jaeger" -o jsonpath="{.items[0].metadata.name}")"
+export POD_NAME
+kubectl port-forward --namespace istio-system "$POD_NAME" 8280:16686 &
+echo $! > /tmp/jaeger-pid
